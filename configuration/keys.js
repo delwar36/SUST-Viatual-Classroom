c80067b1f87eba5e12ccb1fcdfa27695acb92bb5
MijanurRahman40 <mijanurrahman31416@gmail.com>
2020-07-23 18:12:00 +0600

    database into local

--- configuration/keys.js
+++ configuration/keys.js
@@ -1,6 +1,6 @@
-// database = 'mongodb://127.0.0.1:27017/my_project';
+const uri = 'mongodb://127.0.0.1:27017/my_project';
 
-const uri = "mongodb+srv://admin:PNuwLShiRfvpY6SS@cluster0.vjm19.mongodb.net/<sust-virtual-classroom>?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://admin:PNuwLShiRfvpY6SS@cluster0.vjm19.mongodb.net/<sust-virtual-classroom>?retryWrites=true&w=majority";
 
 module.exports = {
     mongoURI: uri
